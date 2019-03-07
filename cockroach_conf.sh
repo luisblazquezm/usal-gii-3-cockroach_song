1ffb887b18d7db76cd41ca98f6b2f289257cbd78
luisblazquezm <luisblazquezm@usal.es>
2019-03-07 17:56:12 +0100

    Commit 2

--- cockroach_conf.sh
+++ cockroach_conf.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+TIME=3
+
 PACKAGE=apt
 
 dpkg -s $PACKAGE  &> /dev/null
@@ -15,11 +17,25 @@ dpkg -s $PACKAGE  &> /dev/null
 
 # Check if package 'beep' is installed
 if [ $? -ne 0 ]; then 
-    echo -e '\n ---- Package $PACKAGE NOT installed. Installing, please wait a few seconds... ---- \n'
+    echo -e "\n ---- Package $PACKAGE NOT installed. Installing, please wait a few seconds... ---- \n"
     sudo apt install $PACKAGE &> /dev/null
-    echo -e '\n ---- Package $PACKAGE succesfully installed. ---- \n'
+    echo -e "\n ---- Package $PACKAGE succesfully installed. ---- \n"
 else 
-    echo -e '\n ---- Package $PACKAGE already installed. Rebooting... \n'
-    sleep 3
-    reboot
+    echo -e "\n ---- Package $PACKAGE already installed. \n"
+fi
+
+read -p '¿Do you want to reboot now?[y/N]:' OPTION
+
+while ! [[ "$OPTION" == 'y' || "$OPTION" == 'Y' || "$OPTION" == 'n' || "$OPTION" == 'N' ]]
+do
+    echo -e "\n Invalid option. Please type y/Y/n/N."
+    read -p '¿Do you want to reboot now?[y/N]:' OPTION
+done
+
+if [[ "$OPTION" == 'y' || "$OPTION" == 'Y' ]]; then
+    echo -e "\nRebooting...\n"
+    sleep $TIME
+    sudo reboot
+elif [[ "$OPTION" == 'n' || "$OPTION" == 'N' ]]; then
+    echo -e "\n Cancelling rebooting. Don't forget to reboot later to test the service. \n"
 fi
